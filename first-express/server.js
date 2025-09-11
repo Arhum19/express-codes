ceeffee48d926c92cbb5ffd217e8c9f57c6f44cc
Arhum19 <arhumnoor10@gmail.com>
2025-09-12 01:33:27 +0500

    Initial commit with .gitignore

--- first-express/server.js
+++ first-express/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 let names = [
-  { id: 1, name: "arhum" },
-  { id: 2, name: "ali" },
-  { id: 3, name: "ahmed" },
-  { id: 4, name: "ahsan" },
+  { id: 1, name: "Arhum" },
+  { id: 2, name: "Ali" },
+  { id: 3, name: "Ahmed" },
+  { id: 4, name: "Ahsan" },
 ];
 //home
 app.get("/", (req, res) => {
@@ -17,27 +17,20 @@ app.get("/users", (req, res) => {
   res.send(user);
 });
 
-//get user from id 
-app.get("/user/:id", (req, res) => {
-  const user = names.find((n) => n.id === parseInt(req.params.id));
-  if (user) {
-    res.send(`Hello ${user.name} From Express!!`);
-  } else {
-    res.status(404).send("User not found");
+//get user from id
+app.get("/user/:value", (req, res) => {
+  const value = req.params.value.toLowerCase();
+
+  let user = names.find((n) => n.id === parseInt(value)); // check by id
+  if (!user) {
+    user = names.find((n) => n.name.toLowerCase() === value); // check by name
   }
-});
-//user from name
-app.get("/about/:name", (req, res) => {
-  const user = names.find((n) => n.name === req.params.name);
-  if (user) {
-    res.send(`My name is ${user.name}!!`);
-  } else {
-    res.status(404).send("User not found");
-  }
+
+  user
+    ? res.send(`Hello ${user.name} From Express!!`)
+    : res.status(404).send("User not found");
 });
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
-
-
