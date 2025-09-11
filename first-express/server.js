ed70a7ec22d63a53856ef1a000ccc7986c136456
Arhum19 <arhumnoor10@gmail.com>
2025-09-12 00:37:26 +0500

    Initial commit with .gitignore

--- first-express/server.js
+++ first-express/server.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const app = express();
-
+let name = "Arhum Noor";
 app.get("/", (req,res)=>{
     res.send("Hello From Express!!");
+})
+app.get("/user", (req,res)=>{
+    res.send(`Hello ${name} From Express!!`);
+})
+app.get("/about", (req,res)=>{
+    res.send("My name is Muhammad Arhum!!");
 })
 
 app.listen(3000, ()=>{
