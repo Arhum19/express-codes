fd4cf8b95ff7ebe975c0d4830a135e7cbf6bef8e
Arhum19 <arhumnoor10@gmail.com>
2025-09-30 02:04:02 +0500

    authentication file edit

--- authentication/app.js
+++ authentication/app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
+let currentuser;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 
 const users = [
-  { username: "arhum", password: "1234", role: "admin" },
+  { username: "arhum", password: "123", role: "admin" },
   { username: "bassam", password: "456", role: "user" },
 ];
 
@@ -20,10 +20,37 @@ app.get("/login", (req, res) => {
   res.render("login", { error: null });
 });
 
-app.post("/login", (req, res) => {});
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const userfind = users.find((user) => {
+    return user.username === username && user.password === password;
+  });
+
+  res.cookie("UserData", JSON.stringify({ username, role: userfind.role }), {
+    maxAge: 2 * 60 * 60 * 1000, // 2 hours
+    httpOnly: true,
+    secure: false, // Set to true if using HTTPS
+    sameSite: "strict",
+  });
+
+  if (userfind) {
+    currentuser = userfind.username;
+    res.redirect("/dashboard");
+  } else {
+    res.render("login", { error: "Invalid username or password" });
+  }
+});
 
 app.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+  if (!currentuser) {
+    return res.redirect("/login");
+  }
+  res.render("dashboard", { username: currentuser });
+});
+
+app.get("/logout", (req, res) => {
+  currentuser = null;
+  res.redirect("/login");
 });
 
 app.listen(port, () => {
